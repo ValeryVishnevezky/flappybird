b59780c3bbbce64da124eee9eddb430bac9af641
Valery Vishnevezky <valery020201@gmail.com>
2025-10-31 00:28:53 +0200

    Add a pipes and movement also add gameover check but the func needs some improvement

--- js/controller.js
+++ js/controller.js
@@ -1,10 +1,16 @@
 let gBirdHeight
+let gPipeRight
 let gGameInterval
+let gPipeInterval
+let gPipeMoveInterval
+let isGameOn
 
 function onInit() {
 	renderGame()
-	gBirdHeight = document.querySelector('.game-action').offsetHeight / 2
-	// document.querySelector('.game-bird').style.bottom = gBirdHeight
+	gBirdHeight = 500
+	gPipeRight = 0
+	isGameOn = true
+	setPipeHeight()
 }
 
 function renderStart() {
@@ -13,7 +19,9 @@ function renderStart() {
 
 function renderGame() {
 	let strHTML = `
-		<img class="game-bird" src="img/flappy-bird.png" alt="flappy bird" />
+		<img class="bird" src="img/flappy-bird.png" alt="flappy bird" />
+			<div class="pipe-top"></div>
+			<div class="pipe-bottom"></div>
 	`
 	document.querySelector('.start-page').classList.add('hidden')
 	let game = document.querySelector('.game-action')
@@ -21,26 +29,102 @@ function renderGame() {
 	game.classList.remove('hidden')
 }
 
-function onGameAreaClick(ev) {
-	ev.preventDefault()
+function onGameAreaClick() {
+	if (!isGameOn) return
 	clearInterval(gGameInterval)
 
-	const gameBird = document.querySelector('.game-bird')
+	const gameBird = document.querySelector('.bird')
 	gameBird.classList.remove('fall')
 	gameBird.style.transform = 'rotate(-40deg)'
-	
-	gBirdHeight += 180
+
+	gBirdHeight += 150
+	console.log('gBirdHeight', gBirdHeight)
 	gameBird.style.bottom = gBirdHeight + 'px'
-	
+
 	setTimeout(() => {
 		gameBird.classList.add('fall')
 	}, 300)
 
 	gGameInterval = setInterval(() => {
-		gBirdHeight -= 1
+		gBirdHeight -= 3
 		gameBird.style.bottom = gBirdHeight + 'px'
+
+		if (checkGameOver(document.querySelectorAll('.pipe-top, .pipe-bottom'))) return	
 		if (gBirdHeight <= 120) {
+			isGameOn = false
 			clearInterval(gGameInterval)
+			clearInterval(gPipeInterval)
+			clearInterval(gPipeMoveInterval)
 		}
-	}, 1)
+	}, 16)
+}
+
+function setPipeHeight() {
+	if (!isGameOn) return
+
+	const pipes = document.querySelectorAll('.pipe-top, .pipe-bottom')
+	pipes.forEach(pipe => {
+		pipe.style.height = getRandomInt(100, 300) + 'px'
+	})
+	gPipeInterval = setInterval(startPipeMovement, 1000)
+}
+
+function startPipeMovement() {
+	if (!isGameOn) return
+
+	gPipeMoveInterval = setInterval(() => {
+		if (!isGameOn) return
+
+		const pipes = document.querySelectorAll('.pipe-top, .pipe-bottom')
+		gPipeRight += 3
+
+		pipes.forEach(pipe => {
+			pipe.style.right = gPipeRight + 'px'
+		})
+
+		if (checkGameOver(pipes)) return
+
+		if (gPipeRight >= 710) {
+			gPipeRight = 0
+			clearInterval(gPipeInterval)
+			clearInterval(gPipeMoveInterval)
+
+			setPipeHeight()
+		}
+	}, 16)
+}
+
+function checkGameOver(pipes) {
+	const game = document.querySelector('.game-action')
+	const gameHeight = game.offsetHeight
+	const bird = document.querySelector('.bird')
+	const birdHeight = bird.offsetHeight
+	const birdTop = gBirdHeight - birdHeight
+	const birdBottom = gBirdHeight
+	const birdRight = 450
+	const birdLeft = 450 - bird.offsetWidth
+
+	const pipeTopHeight = gameHeight - pipes[0].offsetHeight
+	const pipeBottomStart = pipes[1].offsetHeight
+	const pipeLeft = gPipeRight
+	const pipeRight = gPipeRight + 80
+
+	if ((birdTop >= pipeTopHeight || birdBottom <= pipeBottomStart) && (birdRight >= pipeLeft || birdLeft <= pipeRight)) {
+		console.log(`birdTop ${birdTop} >= pipeTopHeight ${pipeTopHeight}`, birdTop >= pipeTopHeight)
+		console.log(`birdBottom ${birdBottom} <= pipeBottomStart ${pipeBottomStart}`, birdBottom <= pipeBottomStart)
+		console.log(`birdRight ${birdRight} >= pipeLeft ${pipeLeft}`, birdRight >= pipeLeft)
+		console.log(`birdLeft ${birdLeft} <= pipeRight ${pipeRight}`, birdLeft <= pipeRight)
+		isGameOn = false
+		console.log('isGameOn', isGameOn)
+		clearInterval(gGameInterval)
+		clearInterval(gPipeInterval)
+		clearInterval(gPipeMoveInterval)
+		return true
+	}
+}
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min)
+	max = Math.floor(max)
+	return Math.floor(Math.random() * (max - min)) + min
 }
